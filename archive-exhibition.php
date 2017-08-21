1498f89b713bea62ab75995539becbfdf2806912
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-08-21 12:57:42 -0500

    wrap archive item in link

--- archive-exhibition.php
+++ archive-exhibition.php
@@ -48,7 +48,7 @@ if ($query->have_posts()) {
     $background_color = get_post_meta($post->ID, '_igv_exhibition_background_color', true);
     $font_color = get_post_meta($post->ID, '_igv_exhibition_font_color', true);
 ?>
-
+        <a href="<?php echo get_the_permalink($post->ID); ?>">
         <article <?php post_class('padding-top-basic padding-bottom-basic'); ?> id="post-<?php the_ID(); ?>" style="background-color: <?php echo $background_color; ?>; color: <?php echo $font_color; ?>;">
           <div class="container">
             <div class="grid-row">
@@ -69,7 +69,7 @@ if ($query->have_posts()) {
 <?php
     }
 ?>
-                  <h2 class="font-serif font-size-large"><a href="<?php echo get_the_permalink($post->ID); ?>"><?php echo get_the_title($post->ID); ?></a></h2>
+                  <h2 class="font-serif font-size-large"><?php echo get_the_title($post->ID); ?></h2>
                 </div>
                 <div class="grid-item item-s-12 item-m-6">
 <?php
@@ -101,6 +101,7 @@ if ($query->have_posts()) {
             </div>
           </div>
         </article>
+        </a>
 
 <?php
   }
